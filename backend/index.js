52a7c888f37a553802083c7cb6725ae8d6bde6a7
Gaurav <>
2026-03-23 19:01:31 +0530

    websocket working properly 8

--- backend/index.js
+++ backend/index.js
@@ -41,13 +41,7 @@ dbConnect()
     //     res.sendFile(path.join(__dirname, "..", "frontend", "dist", "index.html"));
     // });
 // }
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
 
 server.listen(port,function (){
     console.log(`server is started at port number ${port}`);

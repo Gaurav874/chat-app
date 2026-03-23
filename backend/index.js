6fc3e0166b7c836ccb4c13c308282d1d862ade8f
Gaurav <>
2026-03-23 18:31:29 +0530

    websocket working properly 6

--- backend/index.js
+++ backend/index.js
@@ -36,7 +36,7 @@ if(process.env.NODE_ENV === "production") {
     // app.get("*", (req, res) => {
     //     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     // });
-    app.get("/*", (req, res) => {
+    app.get("/(.*)", (req, res) => {
         res.sendFile(path.join(__dirname, "..", "frontend", "dist", "index.html"));
     });
 }
